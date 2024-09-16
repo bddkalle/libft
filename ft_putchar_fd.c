bf0b814721661720551e8f38e8e4c0c2f92707f7
Felix <felix.schnorr@mailbox.org>
2024-09-16 16:27:01 +0200

    updated ft_putchar_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: fschnorr <fschnorr@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:09:55 by fschnorr          #+#    #+#             */
-/*   Updated: 2024/09/05 15:14:30 by fschnorr         ###   ########.fr       */
+/*   Updated: 2024/09/16 16:23:40 by fschnorr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,6 @@
 
 int	ft_putchar_fd(char c, int fd)
 {
-	if (c)
-	{
-		write(fd, &c, 1);
-		return (1);
-	}
-	else
-		return (0);
+	write(fd, &c, 1);
+	return (1);
 }
