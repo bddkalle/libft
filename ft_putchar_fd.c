2dbee78d0761c996b985d348289f8e47ca0792e5
Felix <felix.schnorr@mailbox.org>
2024-09-16 22:54:59 +0200

    norm typo

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: fschnorr <fschnorr@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:09:55 by fschnorr          #+#    #+#             */
-/*   Updated: 2024/09/16 22:37:47 by fschnorr         ###   ########.fr       */
+/*   Updated: 2024/09/16 22:42:12 by fschnorr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_putchar_fd(char c, int fd)
 {
 	int	ret;
 
-	ret = 0;	
+	ret = 0;
 	ret = write(fd, &c, 1);
 	if (ret == -1)
 		return (-1);
